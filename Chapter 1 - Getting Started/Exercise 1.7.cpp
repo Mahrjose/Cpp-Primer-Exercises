e06fb341a1820a1cb4087c6ee4190f2b5747b151
Mahrjose <mirzamahrabhossain@gmail.com>
2022-04-06 18:33:14 +0600

    Add Solution for `Exercise 1.7`

---
+++ Chapter 1 - Getting Started/Exercise 1.7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Single Line Comment
+/*
+ * comment pairs /*    */ cannot nest.
+ * "cannot nest" is considered source code,
+ * as is the rest of the program
+ */
+
+int main() {
+
+    return 0;
+}
