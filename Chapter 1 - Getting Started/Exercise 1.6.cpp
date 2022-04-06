bc3ffeb3c3749784e67c1e6468ab93a061cbfa1c
Mahrjose <mirzamahrabhossain@gmail.com>
2022-04-06 17:45:17 +0600

    Add Solution for `Exercise 1.6`

---
+++ Chapter 1 - Getting Started/Exercise 1.6.cpp
@@ -0,0 +1,11 @@
+/*
+
+The given program fragment is illigal and give this error,
+
+[error] expected primary-expression before ‘<<’ token
+
+To fix this, we can remove some ";" and make the entire thing in a single line,
+
+std::cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+
+*/
